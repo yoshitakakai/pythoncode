7ac1fe80b753af82725d0596e182c51366397389
yoshitakakai <yoshitakakai.net@gmail.com>
2022-07-17 11:59:48 +0900

    クラス

--- code/training01.py
+++ code/training01.py
@@ -1,8 +1,11 @@
-try:
-    a = input("type a number:")
-    b = input("type another:")
-    a = int(a)
-    b = int(b)
-    print(a / b)
-except (ZeroDivisionError, ValueError):
-    print("Invalid input.")
+class Data:
+    def __init__(self):
+        self.nums = [1, 2, 3, 4, 5]
+
+    def change_data(self, index, n):
+        self.nums[index] = n
+
+data_two = Data()
+data_two.change_data(0, 500)
+print(data_two.nums)
+
