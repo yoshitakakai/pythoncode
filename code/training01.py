d06164fc8111bf17e5fb3f6f85e135f64096f726
yoshitakakai <yoshitakakai.net@gmail.com>
2022-07-21 16:11:21 +0900

    basic.py 追加

--- code/training01.py
+++ code/training01.py
@@ -1,20 +1,6 @@
-class Dog:
-    def __init__(self, name, breed, owner):
-        self.owner = owner
-        self.name = name
-        self.breed = breed
+def f(x):
+    return x * 2
 
-class Person:
-    def __init__(self, name):
-        self.name = name
-
-mick = Person("Mick Jager")
-#Personオブジェクトの引数nameを使う
-#mickにPersonを代入
-stan = Dog("Stanley", "Bulldog", mick)
-#一つの目のオブジェクトの引数(name, breed, owner)を使う
-#stanに代入
-print(stan.owner.name)
-#ownerは一つ目のオブジェクトの引数
-#nameは二つ目のオブジェクトの引数
-#stanのowner(mick)
+f(2)
+result = f(2)
+print(result)
