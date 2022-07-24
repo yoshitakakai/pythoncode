f562f502ee01d7617634837b638e1c2f84376064
甲斐義崇 <yoshitaka_kai@kaiyoshitakanoMacBook-Pro.local>
2022-07-24 14:37:32 +0900

    ダイストラ法 追加

--- code/dijkstra03.py
+++ code/dijkstra03.py
@@ -1,21 +1,22 @@
 import heapq
-def dijkstra(edges, num_v, goal):
+
+def dijkstra(edges, num_v):
     dist = [float('inf')] * num_v
     dist[0] = 0
     q = []
-    heapq.heappush(q, [0, [0]])
+    heapq.heappush(q, [0, 0])
 
     while len(q) > 0:
         _, u = heapq.heappop(q)
         for i in edges[u]:
             if dist[i[0]] > dist[u] + i[1]:
                 dist[i[0]] = dist[u] + i[1]
-                heapq.heappush(q, [dist[u] + i[1], u + [i[0]]])
+                heapq.heappush(q, [dist[u] + i[1], i[0]])
 
     return dist
 
 edges = [
-    [[1, 4], [2,3]],
+    [[1, 4], [2, 3]],
     [[2, 1], [3, 1], [4, 5]],
     [[5, 2]],
     [[4, 3]],
