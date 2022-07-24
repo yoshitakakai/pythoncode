4bc618a49feee47023b452c587dc18a38de5a2e5
甲斐義崇 <yoshitaka_kai@kaiyoshitakanoMacBook-Pro.local>
2022-07-25 08:35:56 +0900

    トレーニング

--- code/training01.py
+++ code/training01.py
@@ -1,6 +1,5 @@
-fruit = ["Apple", "Orange"]
-fruit.append("Pear")
-print(fruit)
-print(fruit[2])
-fruit[1] = 2.3
-print(fruit[1])
+colors = ["blue", "green", "yellow"]
+print(colors)
+item = colors.pop()
+print(item)
+print(colors)
