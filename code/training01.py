a790b15e0255d9a9153e73caa05b38e1b8e14254
甲斐義崇 <yoshitakakai.net@gmail.com>
2022-08-10 17:10:21 +0900

    基礎

--- code/training01.py
+++ code/training01.py
@@ -1,5 +1,7 @@
-import csv
-with open("st.csv", "r") as f:
-    r = csv.reader(f, delimiter=",")
-    for row in r:
-        print(",".join(row))
+x = 3
+if x == 3:
+    print("数値は3です")
+if x % 2 == 0:
+    print("数値は偶数です")
+if x % 2 != 0:
+    print("数値は奇数です")
