7ca02027e9080f72552c8f7e1e47f672f47732e0
甲斐義崇 <yoshitakakai.net@gmail.com>
2022-08-23 14:55:18 +0900

    例外処理

--- code/training01.py
+++ code/training01.py
@@ -1,10 +1,8 @@
-
-def f():
-    x = 1
-    y = 2
-    z = 3
-    print(x)
-    print(y)
-    print(z)
-
-f()
+try:
+    a = input("type a number:")
+    b = input("type another:")
+    a = int(a)
+    b = int(b)
+    print(a/b)
+except (ZeroDivisionError, ValueError):
+    print("Invalid input.")
