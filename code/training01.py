416adeec141d047214da3beb2a63f1f66a7f6ba4
甲斐義崇 <yoshitakakai.net@gmail.com>
2022-09-08 11:42:49 +0900

    ファイル操作

--- code/training01.py
+++ code/training01.py
@@ -1,21 +1,6 @@
-def bellman_ford(edges, num_v):
-    dist = [float('inf') for i in range(num_v)]
-    dist[0] = 0
+import csv
 
-    changed = True
-    while changed:
-        changed = False
-        for edge in edges:
-            if dist[edge[1]] > dist[edge[0]] + edge[2]:
-                dist[edge[1]] = dist[edge[0]] + edge[2]
-                changed = True
-
-    return dist
-
-edges = [
-    [0, 1, 4], [0, 2, 3], [1, 2, 1], [1, 3, 1],
-    [1, 4, 5], [2, 5, 2], [4, 6, 2], [5, 4, 1],
-    [5, 6, 4]
-
-]
-print(bellman_ford(edges, 7))
+with open("st.csv", "r") as f:
+    r = csv.reader(f, delimiter=",")
+    for row in r:
+        print(",".join(row))
--- code/training02.py
+++ code/training02.py
@@ -1,2 +1,36 @@
-import training01
+#書き込みとファイルを閉じる指示
+st = open("st.txt", "w")
+st.write("Hello World!!")
+st.close()
 
+#書き込み後自動でファイルを閉じる
+with open("st.txt", "w") as f:
+    f.write("Hello World!!!")
+
+#読み込み
+with open("st.txt", "r")as f:
+    print(f.read())
+
+#コンテナに入れて読み込む
+my_list = []
+
+with open("st.txt", "r") as f:
+    my_list.append(f.read())
+
+print(my_list)
+
+#CSVファイルに書き出し
+import csv
+
+with open("st.csv", "w", newline='') as f:
+    w = csv.writer(f, delimiter=",")
+    w.writerow(["one", "two", "three"])
+    w.writerow(["four", "five", "six"])
+
+#csvファイルを読み込み
+import csv
+
+with open("st.csv", "r") as f:
+    r = csv.reader(f, delimiter=",")
+    for row in r:
+        print(",".join(row))
