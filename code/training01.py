d7ca7d8cc66ebad9f74c8aef0ef86f1d60164583
甲斐義崇 <yoshitakakai.net@gmail.com>
2022-10-28 12:52:48 +0900

    Boyer-Moore法

--- code/training01.py
+++ code/training01.py
@@ -1,13 +1,23 @@
-# ポリモーフィズムなし
-shapelist = [triangletype, circletype]
-for shape in shapelist:
-# このループによってデータ型の確認を行っている
-    if isinstance(shape, Triangle):
-        shape.drew_triangle()
-    if isinstance(shape.Circle):
-        shape.draw_circle()
+text = list('PYTHON PROGRAMMING')
+pattern = list('PRO')
 
-#ポリモーフィズムを実装
-shapelist = [triangletype, circletype]
-for shape in shapelist:
-    shape.draw()
+skip = {}
+for i in range(len(pattern) - 1):
+    skip[pattern[i]] = len(pattern) - i - 1
+
+i = len(pattern) - 1
+while i < len(text):
+    match = True
+    for j in range(len(pattern)):
+        if text[i - j] != pattern[len(pattern) - 1 - j]:
+            match = False
+            break
+    if match:
+        print(i - len(pattern) + 1)
+        break
+    if text[i] in skip:
+        i += skip[text[i]]
+    else:
+        i += len(pattern)
+
+
--- code/training02.py
+++ code/training02.py
@@ -1,55 +1,12 @@
+text = list('PYTHON PROGRAMMING')
+pattern = list('PRO')
 
-# 親クラス
-class People():
-    def __init__(self, greeting):
-        self._greeting = greeting
-
-    def greeting(self):
-        if self._greeting == "Hello":
-            return "Hi"
-        else:
-            return "What?"
-
-
-# 子クラス１
-class American(People):
-    def greeting(self):
-        if self._greeting == "Hello":
-            return "Hello"
-        else:
-            return "Huh?"
-
-
-# 子クラス２
-class Spanish(People):
-    def greeting(self):
-        if self._greeting == "Hola":
-            return "Hola"
-        else:
-            return "Eh?"
-
-
-# 子クラス３
-class French(People):
-    def greeting(self):
-        if self._greeting == "Bonjour":
-            return "Bonjour"
-        else:
-            return "Hein?"
-
-
-# 呼び出し方を統一
-def greeting_people(obj):
-    return obj.greeting()
-
-
-# 実行
-if __name__ == "__main__":
-    american = American("Hello")
-    print(greeting_people(american))
-
-    spanish = Spanish("Hola")
-    print(greeting_people(spanish))
-
-    french = French("Bonjour")
-    print(greeting_people(french))
+for i in range(len(text)):
+    match = True
+    for j in range(len(pattern)):
+        if text[i + j] != pattern[j]:
+            match = False
+            break
+    if match:
+        print(i)
+        break
