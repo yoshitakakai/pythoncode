520978a00d13aa08ccf87f9c1c76f487566b03fc
甲斐義崇 <yoshitakakai.net@gmail.com>
2023-02-27 20:33:17 +0900

    更新

--- code/tk_app/Tetris.py
+++ code/tk_app/Tetris.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import random
 
 # ブロックの種類と形を定義
-shapes = [
+SHAPES = [
     [(0, 0), (1, 0), (0, 1), (1, 1)], # 四角
     [(0, 0), (1, 0), (2, 0), (3, 0)], # 直線
     [(0, 0), (0, 1), (1, 1), (2, 1)], # L字
@@ -36,11 +36,15 @@ class Tetris:
         self.canvas = tk.Canvas(self.root, width=200, height=400)
         self.canvas.pack()
         self.draw_board()
-        self.root.bind("<Key>", self.key_press)
+        self.root.bind("<Left>", self.key_press)
+        self.root.bind("<Right>", self.key_press)
+        self.root.bind("<Down>", self.key_press)
+        self.root.bind("<Up>", self.key_press)
+        self.root.bind("<space>", self.key_press)
         self.timer()
 
     def new_block(self):
-        shape = random.choice(shapes)
+        shape = random.choice(SHAPES)
         self.block = Block(self.width // 2 - 2, 0, shape)
 
     def move_down(self):
@@ -48,85 +52,86 @@ class Tetris:
         if self.check_collision():
             self.block.y -= 1
             self.freeze()
+            self.clear_rows()
+            self.new_block()
 
-    def move_left(self):
-        self.block.x -= 1
-        if self.check_collision():
-            self.block.x += 1
+    def game_over(self):
+        for x, y in self.block.shape:
+            if self.board[self.block.y + y][self.block.x + x]:
+                self.canvas.create_text(100, 200, text="Game Over", font=("Helvetica", 30))
+                return True
+        return False
 
-    def move_right(self):
-        self.block.x += 1
-        if self.check_collision():
-            self.block.x -= 1
-
-    def rotate_left(self):
-        self.block.rotate_left()
-        if self.check_collision():
-            self.block.rotate_right()
-
-    def rotate_right(self):
-        self.block.rotate_right()
-        if self.check_collision():
+    def key_press(self, event):
+        if event.keysym == "Left":
+            self.move(-1)
+        elif event.keysym == "Right":
+            self.move(1)
+        elif event.keysym == "Down":
+            self.move_down()
+        elif event.keysym == "Up":
             self.block.rotate_left()
+        elif event.keysym == "space":
+            self.block.rotate_right()
+        self.draw_board()
+
+    def move(self, dx):
+        self.block.x += dx
+        if self.check_collision():
+            self.block.x -= dx
 
     def check_collision(self):
         for x, y in self.block.shape:
-            x += self.block.x
-            y += self.block.y
-            if not(0 <= x < self.width and 0 <= y < self.height) or self.board[y][x]:
+            if not 0 <= self.block.x + x < self.width:
+                return True
+            if not 0 <= self.block.y + y < self.height:
+                return True
+            if self.board[self.block.y + y][self.block.x + x]:
                 return True
         return False
 
     def freeze(self):
         for x, y in self.block.shape:
-            self.board[y + self.block.y][x + self.block.x] = 1
-            self.clear_lines()
-            self.new_block()
+            self.board[self.block.y + y][self.block.x + x] = 1
 
-    def clear_lines(self):
-        lines = 0
-        for i in range(self.height - 1, -1, -1):
-            if 0 not in self.board[i]:
-                lines += 1
-                for j in range(i, 0, -1):
-                    self.board[j] = self.board[j - 1][:]
-                    self.board[0] = [0] * self.width
-                    self.score += lines ** 2
+    def clear_rows(self):
+        rows_cleared = 0
+        y = self.height - 1
+        while y >= 0:
+            if all(self.board[y]):
+                for i in range(y, 0, -1):
+                    self.board[i] = self.board[i - 1][:]
+                self.board[0] = [0] * self.width
+                rows_cleared += 1
+            else:
+                y -= 1
+        if rows_cleared > 0:
+            self.score += rows_cleared ** 2
+
+    def draw_block(self, x, y, shape):
+        colors = ["orange", "blue", "red", "green", "purple", "yellow", "cyan"]
+        color = colors[SHAPES.index(shape)]
+        for i, j in shape:
+            self.canvas.create_rectangle((x + j) * 20, (y + i) * 20, (x + j + 1) * 20, (y + i + 1) * 20, fill=color, width=2)
 
     def draw_board(self):
         self.canvas.delete("all")
-        for y in range(self.height):
-            for x in range(self.width):
-                if self.board[y][x]:
-                    self.canvas.create_rectangle(x * 20, y * 20, x * 20 + 20, y * 20 + 20, fill="gray")
-                    for x, y in self.block.shape:
-                        self.canvas.create_rectangle((x + self.block.x) * 20, (y + self.block.y) * 20, (x + self.block.x) * 20 + 20, (y + self.block.y) * 20 + 20, fill="red")
+        for y, row in enumerate(self.board):
+            for x, val in enumerate(row):
+                if val:
+                    self.canvas.create_rectangle(x * 20, y * 20, (x + 1) * 20, (y + 1) * 20, fill="gray", width=2)
+        self.draw_block(self.block.x, self.block.y, self.block.shape)
 
     def timer(self):
-        self.move_down()
-        self.draw_board()
-        if not self.check_game_over():
-            self.root.after(500, self.timer)
-
-    def check_game_over(self):
-        if self.check_collision():
-            self.canvas.create_text(100, 200, text="Game Over", font=("Helvetica", 36))
-            return True
-        else:
-            return False
-
-    def key_press(self, event):
-        key = event.keysym
-        if key == "Left":
-            self.move_left()
-        elif key == "Right":
-            self.move_right()
-        elif key == "Down":
+        if not self.game_over():
             self.move_down()
-        elif key == "Up":
-            self.rotate_left()
-        elif key == "space":
-            self.rotate_right()
-        if name == "main":
-            tetris = Tetris()
-            tetris.root.mainloop()
+            self.draw_board()
+            self.root.after(400, self.timer)
+
+    def run(self):
+        self.root.mainloop()
+
+if __name__ == "__main__":
+    tetris = Tetris()
+    tetris.run()
+
