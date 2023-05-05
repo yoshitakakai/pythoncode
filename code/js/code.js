f529081466f31267fe0fc047594cacc69dc34c13
甲斐義崇 <yoshitakakai.net@gmail.com>
2023-05-05 15:07:16 +0900

    js勉強

--- code/js/code.js
+++ code/js/code.js
@@ -1,34 +1,4 @@
-function displayTrainType() {
-    var station = inputStation();
-    var type = getTrainType(station);
-    if (station >= 1 && station <= 3) {
-        alert("その駅には" + type + "の電車が停まります");
-    } else {
-        displayTrainType();
-    }
-}
-function inputStation() {
-    var station = prompt("行き先を選んで\n1.A 2.B 3.C");
-    station = Number(station);
-    return station;
-}
-
-function getTrainType(station) {
-    var type;
-    switch (station) {
-        case 1:
-            type = "快速";
-            break;
-        case 2:
-            type = "快速と急行";
-            break;
-        case 3:
-            type = "急行";
-            break;
-        default:
-            alert("入力が正しくありません")
-    }
-    return type;
-}
-
-displayTrainType();
+var element = document.querySelector(".weather");
+console.log(element);
+var element = document.querySelector("dl");
+console.log(element);
