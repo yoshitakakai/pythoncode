325304495505f1d5120d80742adefdb7f35041eb
甲斐義崇 <yoshitakakai.net@gmail.com>
2023-05-09 16:13:08 +0900

    js勉強

--- code/js/code.js
+++ code/js/code.js
@@ -1,7 +1,21 @@
-var button = document.getElementById("nice");
-button.addEventListener("click", niceaction)
-function niceaction(){
-    var element = document.createElement("p");
-    var text = document.createTextNode("いいね!");
-    document.body.appendChild(element).appendChild(text);
+const fs = require('fs')
+
+function readFile_pr (fname) {
+  return new Promise((resolve)  => {
+    fs.readFile(fname, 'utf-8', (err, s) => {
+      resolve(s)
+    })
+  })
 }
+
+readFile_pr('a.txt')
+.then((text) => {
+  console.log('a.txtを読んだ', text)
+  return readFile_pr('b.txt')
+}).then((text) => {
+  console.log('b.txtを読んだ', text)
+  return readFile_pr('c.txt')
+})
+.then((text) => {
+  console.log('c.txtを読んだ', text)
+})
